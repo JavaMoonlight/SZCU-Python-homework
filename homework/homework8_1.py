97a23d573068ec2dca67875e72d6162fdfc43e3d
Moonlight <wyt2298182@outlook.com>
2023-04-28 15:23:36 +0800

    The latest submission

--- homework/homework8_1.py
+++ homework/homework8_1.py
@@ -1,6 +1,7 @@
 import re
 
-phoneNumRegex = re.compile(r'(\(\d{3}\))?(\d{3}-)?(\d{3}-\d{4})')
+# phoneNumRegex = re.compile(r'(\(\d{3}\))?(\d{3}-)?(\d{3}-\d{4})')
+phoneNumRegex = re.compile(r'(\(\d{3}\)|(\d{3}-))?\d{3}-\d{4}')
 phone = input('Please enter your telephone number ：')
 mo = phoneNumRegex.search('My number is '+phone)
 print('Phone number found: ',mo.group())
