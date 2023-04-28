37df18e078f672f60972f6f9da84c247a4c579c9
Moonlight <wyt2298182@outlook.com>
2023-04-28 15:26:30 +0800

    For the last code change

--- homework/homework8_2.py
+++ homework/homework8_2.py
@@ -5,6 +5,5 @@ sentence = input('Please enter a sentence: ')
 list = ''
 for i in range(0, len(abbreviation)):
     list += abbreviation[i]+"[a-z]+\s?([a-z]+ )?"
-print(list)
 result = re.search(list, sentence)
 print(result.group())
